1180aa4559aa55cf989b587324d7916120087e02
Vedant Navle <vedantnavale02@gmail.com>
2026-04-14 21:19:27 +0530

    Change bcrypt hash rounds from 12 to 10

--- tablemint-web/tablemint-backend/src/models/User.js
+++ tablemint-web/tablemint-backend/src/models/User.js
@@ -83,7 +83,7 @@ userSchema.pre('save', async function (next) {
 // Hash password
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
